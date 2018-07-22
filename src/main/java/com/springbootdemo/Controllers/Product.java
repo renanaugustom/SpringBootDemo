32ad214c5c164e6cf168fc4c970b4ec0daf2e669
Renan <renan.augusto18@hotmail.com>
2018-07-22 16:24:26 -0300

    Initializing Project

---
+++ src/main/java/com/springbootdemo/Controllers/Product.java
@@ -0,0 +1,40 @@
+package com.springbootdemo.Controllers;
+
+import java.util.Currency;
+
+public class Product {
+
+    private int id;
+    private String name;
+    private Currency value;
+
+    public Product(int id, String name, Currency value) {
+        this.id = id;
+        this.name = name;
+        this.value = value;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Currency getValue() {
+        return value;
+    }
+
+    public void setValue(Currency value) {
+        this.value = value;
+    }
+}
